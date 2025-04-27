62a05c3d36c8e601e2527f8c9fff0576baaa77bf
Tilzit <mylittleanabolik@gmail.com>
2025-04-28 00:07:32 +0200

    advice-card

--- source/js/main.js
+++ source/js/main.js
@@ -30,3 +30,20 @@
     burgerButton.addEventListener("click", onBurgerButtonClick);
     closeBurgerBtn.addEventListener("click", closeMenu);
 })();
+
+//advice-табы
+(function () {
+    const tabs =  Array.prototype.slice.apply(document.querySelectorAll('.tabs__item'));
+    const content =  Array.prototype.slice.apply(document.querySelectorAll('.tabs__content-item'));
+    
+    document.getElementById('advice-tabs').addEventListener('click', e => {
+        if (e.target.classList.contains('tabs__item')) {
+            let tabSelect = tabs.indexOf(e.target);
+            tabs.map( item => item.classList.remove('active'));
+            tabs[tabSelect].classList.add('active');
+            
+            content.map( content => content.classList.remove('active'));
+            content[tabSelect].classList.add('active');
+        }
+    })
+})();
