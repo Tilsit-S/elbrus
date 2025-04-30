accbf03773d38134992cfe87602ef68b426ea18c
Tilzit <mylittleanabolik@gmail.com>
2025-04-30 21:14:22 +0200

    .

--- source/js/main.js
+++ source/js/main.js
@@ -33,15 +33,15 @@
 
 //advice-табы
 (function () {
-    const tabs =  Array.prototype.slice.apply(document.querySelectorAll('.tabs__item'));
-    const content =  Array.prototype.slice.apply(document.querySelectorAll('.tabs__content-item'));
-    
-    document.getElementById('advice-tabs').addEventListener('click', e => {
-        if (e.target.classList.contains('tabs__item')) {
+    const tabs =  Array.prototype.slice.apply(document.querySelectorAll('.advice__tabs-item'));
+    const content =  Array.prototype.slice.apply(document.querySelectorAll('.advice__content-item'));
+
+    document.getElementById('adviceTabs').addEventListener('click', e => {
+        if (e.target.classList.contains('advice__tabs-item')) {
             let tabSelect = tabs.indexOf(e.target);
             tabs.map( item => item.classList.remove('active'));
             tabs[tabSelect].classList.add('active');
-            
+
             content.map( content => content.classList.remove('active'));
             content[tabSelect].classList.add('active');
         }
