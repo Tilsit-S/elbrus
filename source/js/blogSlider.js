44acabfde30a16930d1d1699b7fbf097a4e57222
Tilzit <mylittleanabolik@gmail.com>
2025-05-07 08:32:11 +0200

    main

---
+++ source/js/blogSlider.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $('.blog__slider').slick({
+        arrows: false,
+        dots: false,
+        variableWidth: true,
+        adaptiveHeight: false,
+        infinite: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1743,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    dots: true,
+                },
+            },
+        ],
+    }
+    );
+});
